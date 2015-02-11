a9078d6aa05f5fa66d06d2da25c8d3db40c81e16
Stephen Berquet <stephen.berquet@gmail.com>
2015-02-11 21:12:17 +0100

    updated config

--- api_config.py
+++ api_config.py
@@ -1,6 +1,15 @@
 def getSourceFileList(cryptoppVersion):
     sourceFileList = []
+    sourceFileList.append("src/keying/api_symmetric_iv_abstract.cpp")
+    sourceFileList.append("src/keying/api_symmetric_key_abstract.cpp")
+
     sourceFileList.append("src/symmetric/cipher/block/api_block_cipher_abstract.cpp")
     sourceFileList.append("src/symmetric/cipher/block/api_block_cipher_aes.cpp")
+
+    sourceFileList.append("src/symmetric/cipher/stream/api_stream_cipher_abstract.cpp")
+    sourceFileList.append("src/symmetric/cipher/stream/api_stream_cipher_panama.cpp")
+    sourceFileList.append("src/symmetric/cipher/stream/api_stream_cipher_salsa20.cpp")
+    sourceFileList.append("src/symmetric/cipher/stream/api_stream_cipher_sosemanuk.cpp")
+    sourceFileList.append("src/symmetric/cipher/stream/api_stream_cipher_xsalsa20.cpp")
 
     return sourceFileList
