70a2b90bf64f86a435fd74760910747e232aa0f4
Stephen Berquet <stephen.berquet@gmail.com>
2015-02-12 00:22:56 +0100

    added mac api

---
+++ src/mac/api_mac_abstract.h
@@ -0,0 +1,48 @@
+
+/*
+ * This file is part of cryptopp-bindings-api.
+ *
+ * (c) Stephen Berquet <stephen.berquet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+#ifndef API_CRYPTOPP_MAC_ABSTRACT_H
+#define API_CRYPTOPP_MAC_ABSTRACT_H
+
+#include "api_cryptopp.h"
+#include "api_mac_interface.h"
+
+NAMESPACE_BEGIN(CryptoppApi)
+
+class MacAbstract : public MacInterface
+{
+public:
+    const char *getName() const;
+    size_t getDigestSize() const;
+    size_t getBlockSize() const;
+    bool isValidKeyLength(size_t length) const;
+    void setKey(const byte *key, const size_t keyLength);
+    void getKey(byte *key);
+    size_t getKeyLength() {return m_keyLength;}
+    void calculateDigest(byte *input, size_t inputLength, byte *output);
+    void update(byte *input, size_t inputLength);
+    void finalize(byte *output);
+    void restart();
+
+protected:
+    MacAbstract();
+    void setCryptoppObject(CryptoPP::MessageAuthenticationCode *mac);
+    void setName(const std::string name);
+
+private:
+    char *m_name;
+    CryptoPP::MessageAuthenticationCode *m_mac;
+    byte *m_key;
+    size_t m_keyLength;
+};
+
+NAMESPACE_END
+
+#endif /* API_CRYPTOPP_MAC_ABSTRACT_H */
---
+++ src/mac/api_mac_interface.h
@@ -0,0 +1,26 @@
+
+/*
+ * This file is part of cryptopp-bindings-api.
+ *
+ * (c) Stephen Berquet <stephen.berquet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+#ifndef API_CRYPTOPP_MAC_INTERFACE_H
+#define API_CRYPTOPP_MAC_INTERFACE_H
+
+#include "api_cryptopp.h"
+#include "api_hash_transformation_interface.h"
+#include "src/keying/api_symmetric_key_abstract.h"
+
+NAMESPACE_BEGIN(CryptoppApi)
+
+class MacInterface : public HashTransformationInterface, public SymmetricKeyAbstract
+{
+};
+
+NAMESPACE_END
+
+#endif /* API_CRYPTOPP_MAC_INTERFACE_H */
