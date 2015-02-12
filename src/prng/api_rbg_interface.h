bf53cb0c30aa2a56a052a9b9a25b3e7368fafb23
Stephen Berquet <stephen.berquet@gmail.com>
2015-02-12 21:19:23 +0100

    added prng api

---
+++ src/prng/api_rbg_interface.h
@@ -0,0 +1,27 @@
+
+/*
+ * This file is part of cryptopp-bindings-api.
+ *
+ * (c) Stephen Berquet <stephen.berquet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+#ifndef API_CRYPTOPP_RANDOM_BYTE_GENERATOR_INTERFACE_H
+#define API_CRYPTOPP_RANDOM_BYTE_GENERATOR_INTERFACE_H
+
+#include "src/api_cryptopp.h"
+
+NAMESPACE_BEGIN(CryptoppApi)
+
+class RandomByteGeneratorInterface
+{
+public:
+    virtual ~RandomByteGeneratorInterface() {}
+    virtual void generate(byte *output, size_t size) =0;
+};
+
+NAMESPACE_END
+
+#endif /* API_CRYPTOPP_RANDOM_BYTE_GENERATOR_INTERFACE_H */
