ac72d255a2a880c1e55eec96323c5ea2406aaa29
Stephen Berquet <stephen.berquet@gmail.com>
2015-02-12 21:59:53 +0100

    added ofb mode api

---
+++ src/symmetric/mode/api_symmetric_mode_ofb.cpp
@@ -0,0 +1,37 @@
+
+/*
+ * This file is part of cryptopp-bindings-api.
+ *
+ * (c) Stephen Berquet <stephen.berquet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+#include "api_symmetric_mode_ofb.h"
+#include <string>
+
+NAMESPACE_BEGIN(CryptoppApi)
+
+SymmetricModeOfb::SymmetricModeOfb(BlockCipherInterface *cipher)
+    : SymmetricModeAbstract()
+{
+    // name
+    setName("ofb", cipher->getName());
+
+    // create mode object
+    size_t blockSize = cipher->getBlockSize();
+    byte dummyIv[blockSize];
+
+    m_encryptor = new CryptoPP::OFB_Mode_ExternalCipher::Encryption(cipher->getEncryptor(), dummyIv, blockSize);
+    m_decryptor = new CryptoPP::OFB_Mode_ExternalCipher::Decryption(cipher->getEncryptor(), dummyIv, blockSize);
+    setCryptoppObjects(m_encryptor, m_decryptor);
+}
+
+SymmetricModeOfb::~SymmetricModeOfb()
+{
+    delete m_encryptor;
+    delete m_decryptor;
+}
+
+NAMESPACE_END
---
+++ src/symmetric/mode/api_symmetric_mode_ofb.h
@@ -0,0 +1,34 @@
+
+/*
+ * This file is part of cryptopp-bindings-api.
+ *
+ * (c) Stephen Berquet <stephen.berquet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+#ifndef API_CRYPTOPP_SYMMETRIC_MODE_OFB_H
+#define API_CRYPTOPP_SYMMETRIC_MODE_OFB_H
+
+#include "src/api_cryptopp.h"
+#include "src/symmetric/cipher/block/api_block_cipher_interface.h"
+#include "api_symmetric_mode_abstract.h"
+#include <modes.h>
+
+NAMESPACE_BEGIN(CryptoppApi)
+
+class SymmetricModeOfb : public SymmetricModeAbstract
+{
+public:
+    SymmetricModeOfb(BlockCipherInterface *cipher);
+    ~SymmetricModeOfb();
+
+private:
+    CryptoPP::OFB_Mode_ExternalCipher::Encryption *m_encryptor;
+    CryptoPP::OFB_Mode_ExternalCipher::Decryption *m_decryptor;
+};
+
+NAMESPACE_END
+
+#endif /* API_CRYPTOPP_SYMMETRIC_MODE_OFB_H */
