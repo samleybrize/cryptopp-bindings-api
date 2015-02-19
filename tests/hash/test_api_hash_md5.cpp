4dc2abae85c9ad0c5fc6e4637daa9a9cc183ba30
Stephen Berquet <stephen.berquet@gmail.com>
2015-02-19 21:10:45 +0100

    added hash md5 unit tests

--- tests/hash/test_api_hash_md5.cpp
+++ tests/hash/test_api_hash_md5.cpp
@@ -11,8 +11,13 @@
 #include "tests/test_api_assertions.h"
 #include <gtest/gtest.h>
 
-// TODO is instanceof HashInterface
-// TODO is instanceof HashAbstract
+TEST(HashMd5Test, inheritance) {
+    CryptoppApi::HashMd5 hash;
+
+    EXPECT_TRUE(0 != dynamic_cast<CryptoppApi::HashTransformationInterface*>(&hash));
+    EXPECT_TRUE(0 != dynamic_cast<CryptoppApi::HashInterface*>(&hash));
+    EXPECT_TRUE(0 != dynamic_cast<CryptoppApi::HashAbstract*>(&hash));
+}
 
 TEST(HashMd5Test, infos) {
     CryptoppApi::HashMd5 hash;
@@ -22,7 +27,134 @@ TEST(HashMd5Test, infos) {
     EXPECT_EQ(16, hash.getDigestSize());
 }
 
-// TODO calculateDigest
-// TODO update
-// TODO finalize
-// TODO restart
+TEST(HashMd5Test, calculateDigest) {
+    CryptoppApi::HashMd5 hash;
+
+    // build expected digests
+    byte *expected1;
+    byte *expected2;
+    size_t expected1Length = 0;
+    size_t expected2Length = 0;
+    hex2bin("6eea9b7ef19179a06954edd0f6c05ceb", 32, &expected1, expected1Length);
+    hex2bin("ab4f63f9ac65152575886860dde480a1", 32, &expected2, expected2Length);
+
+    // calculate actual digests
+    char *input1        = "qwertyuiop";
+    char *input2        = "azerty";
+    size_t digestSize   = hash.getDigestSize();
+    byte actual1[digestSize];
+    byte actual2[digestSize];
+
+    hash.calculateDigest(reinterpret_cast<byte*>(input1), strlen(input1), actual1);
+    hash.calculateDigest(reinterpret_cast<byte*>(input2), strlen(input2), actual2);
+
+    // test digests
+    EXPECT_BYTE_ARRAY_EQ(expected1, expected1Length, actual1, digestSize);
+    EXPECT_BYTE_ARRAY_EQ(expected2, expected2Length, actual2, digestSize);
+
+    delete[] expected1;
+    delete[] expected2;
+}
+
+TEST(HashMd5Test, update) {
+    CryptoppApi::HashMd5 hash;
+
+    // build expected digest
+    byte *expected;
+    size_t expectedLength = 0;
+    hex2bin("6eea9b7ef19179a06954edd0f6c05ceb", 32, &expected, expectedLength);
+
+    // calculate actual digest
+    char *input1        = "qwerty";
+    char *input2        = "uio";
+    char *input3        = "p";
+    size_t digestSize   = hash.getDigestSize();
+    byte actual[digestSize];
+
+    hash.update(reinterpret_cast<byte*>(input1), strlen(input1));
+    hash.update(reinterpret_cast<byte*>(input2), strlen(input2));
+    hash.update(reinterpret_cast<byte*>(input3), strlen(input3));
+
+    hash.finalize(actual);
+
+    // test digests
+    EXPECT_BYTE_ARRAY_EQ(expected, expectedLength, actual, digestSize);
+
+    delete[] expected;
+}
+
+TEST(HashMd5Test, restartNotNecessaryAfterFinalize) {
+    CryptoppApi::HashMd5 hash;
+
+    // build expected digest
+    byte *expected;
+    size_t expectedLength = 0;
+    hex2bin("6eea9b7ef19179a06954edd0f6c05ceb", 32, &expected, expectedLength);
+
+    // calculate actual digest
+    char *input1        = "qwerty";
+    char *input2        = "uio";
+    char *input3        = "p";
+    size_t digestSize   = hash.getDigestSize();
+    byte actual[digestSize];
+
+    hash.update(reinterpret_cast<byte*>(input1), strlen(input1));
+    hash.update(reinterpret_cast<byte*>(input1), strlen(input1));
+    hash.finalize(actual);
+
+    hash.update(reinterpret_cast<byte*>(input1), strlen(input1));
+    hash.update(reinterpret_cast<byte*>(input2), strlen(input2));
+    hash.update(reinterpret_cast<byte*>(input3), strlen(input3));
+
+    hash.finalize(actual);
+
+    // test digests
+    EXPECT_BYTE_ARRAY_EQ(expected, expectedLength, actual, digestSize);
+
+    delete[] expected;
+}
+
+TEST(HashMd5Test, restart) {
+    CryptoppApi::HashMd5 hash;
+
+    // build expected digest
+    byte *expected;
+    size_t expectedLength = 0;
+    hex2bin("6eea9b7ef19179a06954edd0f6c05ceb", 32, &expected, expectedLength);
+
+    // calculate actual digest
+    char *input1        = "qwerty";
+    char *input2        = "uio";
+    char *input3        = "p";
+    size_t digestSize   = hash.getDigestSize();
+    byte actual[digestSize];
+
+    hash.update(reinterpret_cast<byte*>(input1), strlen(input1));
+    hash.update(reinterpret_cast<byte*>(input1), strlen(input1));
+    hash.restart();
+
+    hash.update(reinterpret_cast<byte*>(input1), strlen(input1));
+    hash.update(reinterpret_cast<byte*>(input2), strlen(input2));
+    hash.update(reinterpret_cast<byte*>(input3), strlen(input3));
+
+    hash.finalize(actual);
+
+    // test digests
+    EXPECT_BYTE_ARRAY_EQ(expected, expectedLength, actual, digestSize);
+
+    delete[] expected;
+}
+
+TEST(HashMd5Test, largeData) {
+    CryptoppApi::HashMd5 hash;
+
+    size_t digestSize   = hash.getDigestSize();
+    byte *input         = new byte[10485760];
+    byte output[digestSize];
+
+    hash.calculateDigest(input, 10485760, output);
+    hash.update(input, 10485760);
+    hash.finalize(output);
+
+    delete[] input;
+}
