c0709b644d7c37e83623f26f7b5fecac2ded5171
Stephen Berquet <stephen.berquet@gmail.com>
2015-03-11 14:46:01 +0100

    added an assertion to test an exception message

--- tests/test_api_assertions.h
+++ tests/test_api_assertions.h
@@ -12,7 +12,9 @@
 
 #include "src/api_cryptopp.h"
 #include <gtest/gtest.h>
+#include <string>
 
+// byte array comparison
 #define EXPECT_BYTE_ARRAY_EQ(expected, expectedSize, actual, actualSize) GTEST_ASSERT_(ByteArrayEquals(expected, expectedSize, actual, actualSize), GTEST_NONFATAL_FAILURE_);
 #define EXPECT_BYTE_ARRAY_NE(notExpected, expectedSize, actual, actualSize) GTEST_ASSERT_(ByteArrayNotEquals(notExpected, expectedSize, actual, actualSize), GTEST_NONFATAL_FAILURE_);
 #define ASSERT_BYTE_ARRAY_EQ(expected, expectedSize, actual, actualSize) GTEST_ASSERT_(ByteArrayEquals(expected, expectedSize, actual, actualSize), GTEST_FATAL_FAILURE_);
@@ -20,5 +22,46 @@
 
 ::testing::AssertionResult ByteArrayEquals(const byte *expected, const size_t expectedSize, const byte *actual, const size_t actualSize);
 ::testing::AssertionResult ByteArrayNotEquals(const byte *expected, const size_t expectedSize, const byte *actual, const size_t actualSize);
+
+// exception message test
+#define EXPECT_THROW_MSG(statement, expected_exception, msg) \
+    TEST_THROW_MSG_(statement, expected_exception, msg, GTEST_NONFATAL_FAILURE_)
+#define ASSERT_THROW_MSG(statement, expected_exception, msg) \
+    TEST_THROW_MSG_(statement, expected_exception, msg, GTEST_FATAL_FAILURE_)
+
+#define TEST_THROW_MSG_(statement, expected_exception, expected_msg, fail) \
+    GTEST_AMBIGUOUS_ELSE_BLOCKER_ \
+    if (::testing::internal::ConstCharPtr gtest_msg = "") { \
+        bool gtest_caught_expected = false; \
+        try { \
+            GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement); \
+        } catch (expected_exception const&e) { \
+            gtest_caught_expected = true; \
+            if (0 != strcmp(expected_msg, e.what())) { \
+                std::string msg("Expected: " #statement " throws an exception of type " #expected_exception " with message '"); \
+                msg.append(expected_msg); \
+                msg.append("'.\n"); \
+                msg.append("  Actual: message is '"); \
+                msg.append(e.what()); \
+                msg.append("'."); \
+                /* using the label cause the first message to not appear. This is a workaround */ \
+                fail(msg.c_str()); \
+                break; \
+            } \
+        } catch (...) { \
+            gtest_msg.value = \
+                "Expected: " #statement " throws an exception of type " \
+                #expected_exception ".\n  Actual: it throws a different type."; \
+            goto GTEST_CONCAT_TOKEN_(gtest_label_testthrow_, __LINE__); \
+        } \
+        if (!gtest_caught_expected) { \
+            gtest_msg.value = \
+                "Expected: " #statement " throws an exception of type " \
+                #expected_exception ".\n  Actual: it throws nothing."; \
+            goto GTEST_CONCAT_TOKEN_(gtest_label_testthrow_, __LINE__); \
+        } \
+    } else \
+        GTEST_CONCAT_TOKEN_(gtest_label_testthrow_, __LINE__): \
+            fail(gtest_msg.value)
 
 #endif /* TEST_API_CRYPTOPP_ASSERTIONS_H */
